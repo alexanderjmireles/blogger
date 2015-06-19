0e79c5738fd0f260ed183b7f8e8ed52203c51071
alexanderjmireles <alexandermireles0gmail.com>
2015-06-19 16:07:57 -0700

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,18 @@ class ArticlesController < ApplicationController
 
 	end
 
+	def edit
+  	@article = Article.find(params[:id])
+	end
+	def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+ flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+	end
+
 
 end
 
