ad6de61d1d5459954a5e85e8996e513d0051cd27
alexanderjmireles <alexandermireles0@gmail.com>
2015-06-26 14:53:56 -0700

    tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,16 @@ class Article < ActiveRecord::Base
 	has_many :taggings
 	has_many :tags, through: :taggings
 
+	def tag_list
+		tags = []
+		self.tags.each do |tag|
+			tags.push tag.name
+		end
+		tags.join(", ")
+	end
+
+
+	def tag_list=(tags_string)
+	end
 end
+
